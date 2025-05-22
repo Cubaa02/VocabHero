3d7e9178ba98f0d5a74a6a1b8038289a53438dbd
Cubaa02 <jakub.plucnar@gmail.com>
2025-05-22 10:03:47 +0200

    Databaze

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('game/', views.game_view, name='game'),
+    path('words/', views.word_list, name='word_list'),
 ]
--- core/views.py
+++ core/views.py
@@ -7,3 +7,7 @@ from .models import Word
 def game_view(request):
     word = random.choice(Word.objects.all())  # náhodné slovo z databáze
     return render(request, 'core/game.html', {'word': word})
+
+def word_list(request):
+    words = Word.objects.all()
+    return render(request, 'core/word_list.html', {'words': words})
